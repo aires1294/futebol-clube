be2bad1f78427a2ce0184ea21b83cf1188a629db
aires1294 <pedrotja.engenharia@gmail.com>
2023-04-04 15:59:57 -0300

    item 10 feitooo

--- app/backend/src/Authentication/middlewares.ts
+++ app/backend/src/Authentication/middlewares.ts
@@ -1,4 +1,5 @@
 // import UsersService from 'src/services/User';
+// import * as bcrypt from 'bcryptjs';
 import { Request, Response, NextFunction } from 'express';
 
 const validateLoginBody = (req: Request, res: Response, next: NextFunction) => {
@@ -14,10 +15,25 @@ const validateEmail = async (req: Request, res: Response, next: NextFunction) =>
   const regex = /\S+@\S+\.\S+/g;
   const emailConfirmed = regex.test(email);
   if (!emailConfirmed) {
-    return res.status(400)
-      .json({ message: '"email" must be a valid email' });
+    return res.status(401).json({ message: 'Invalid email or password' });
   }
   next();
 };
 
-export { validateLoginBody, validateEmail };
+// const validatePassword = async (req: Request, res: Response, next: NextFunction) => {
+//   const { password } = req.body;
+//   const verifyPassword = bcrypt.compareSync(password, this.password);
+//   if (!verifyPassword) {
+//     return { status: 401, message: 'Invalid email or password' };
+//   }
+// };
+
+const validatePassword = async (req: Request, res: Response, next: NextFunction) => {
+  const { password } = req.body;
+  if (password.length < 6) {
+    return res.status(401).json({ message: 'Invalid email or password' });
+  }
+  next();
+};
+
+export { validateLoginBody, validateEmail, validatePassword };
--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -1,7 +1,7 @@
 import * as express from 'express';
 import TeamsController from './controllers/Teams';
 import UsersController from './controllers/User';
-import { validateLoginBody } from './Authentication/middlewares';
+import { validateLoginBody, validateEmail, validatePassword } from './Authentication/middlewares';
 // import TeamRouter from './Router/Teams';
 
 class App {
@@ -28,7 +28,13 @@ class App {
     this.app.use(accessControl);
     this.app.get('/teams', TeamsController.getTeams);
     this.app.get('/teams/:id', TeamsController.getTeamById);
-    this.app.post('/login', validateLoginBody, UsersController.login);
+    this.app.post(
+      '/login',
+      validateLoginBody,
+      validateEmail,
+      validatePassword,
+      UsersController.login,
+    );
   }
 
   public start(PORT: string | number):void {
