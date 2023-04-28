eed5cf6a8a903cd13fe206b181cb9ddbed14f9ec
aires1294 <pedrotja.engenharia@gmail.com>
2023-04-28 10:12:49 -0300

    fix

--- app/backend/src/Router/Leaderboard.ts
+++ app/backend/src/Router/Leaderboard.ts
@@ -1,5 +1,5 @@
-import { Router, Request, Response } from 'express';
+// import { Router, Request, Response } from 'express';
 
-const LeaderboardRouter = Router();
+// const LeaderboardRouter = Router();
 
-export default LeaderboardRouter;
+// export default LeaderboardRouter;
